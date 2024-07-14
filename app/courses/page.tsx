36964ad11e1ec2cb70e8e8dd421c633416f50fef
volfcan <20183360068@ogr.akdeniz.edu.tr>
2024-07-14 20:13:06 +0300

    🔺feat: layout added

---
+++ app/courses/page.tsx
@@ -0,0 +1,9 @@
+import PageWrapper from "@/components/wrapper/page-wrapper";
+
+export default function CoursesPage() {
+  return (
+    <PageWrapper>
+      <div>Courses Page </div>
+    </PageWrapper>
+  );
+}
