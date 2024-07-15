7f738f17b4fa3589c379cc80ff70738c23db912a
Ramakrushna Mohapatra <itsramakrushna@gmail.com>
2024-07-15 09:50:21 +0000

    courses: layout added

---
+++ components/wrapper/page-wrapper.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { FloatingNav } from "../ui/FloatingNavbar";
+import Footer from "../Footer";
+import { navItems } from "@/data";
+
+export default function PageWrapper({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <FloatingNav navItems={navItems} />
+      <main className="flex min-w-screen flex-col pt-[12rem] items-center dark:bg-black bg-white justify-between">
+        {/* <div className="absolute z-[-99] pointer-events-none inset-0 flex items-center justify-center "></div> */}
+        {children}
+      </main>
+      <Footer />
+    </>
+  );
+}
